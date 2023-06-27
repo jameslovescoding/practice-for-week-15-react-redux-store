1da7d90fe9f23cd3092ef793e84d18b2b801c4b0
James Yu Cao <caoyu19920414@gmail.com>
2023-06-27 17:12:27 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+import configureStore from './store';
+import { Provider } from 'react-redux';
+
+const store = configureStore();
+
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
